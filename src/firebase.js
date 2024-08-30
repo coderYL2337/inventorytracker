237c7088ef24356ba8bb559ef7e9440dcb981431
coderYL2337 <coderyl2337@outlook.com>
2024-08-30 00:41:49 -0400

    cleaned

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { collection, addDoc } from "firebase/firestore"; 
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported} from "firebase/analytics";
 import { env } from 'process';
 
 const firebaseConfig = {
@@ -14,20 +14,17 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
-export {firestore};
 
-// const firebaseConfig = {
-//  apiKey: "AIzaSyAQShQqUjhYDfp8Zo1O9xfRv78_lf2W8p4",
-//  authDomain: "inventory-management-app-d9849.firebaseapp.com",
-//  projectId: "inventory-management-app-d9849",
-//  storageBucket: "YOUR_PROJECT_ID.appspot.cominventory-management-app-d9849.appspot.com",
-//  messagingSenderId: "83040293798",
-//  appId: "1:83040293798:web:5ccfa00d7b43ddf1377567"
-//  };
-// const app = initializeApp(firebaseConfig);
-// const firestore = getFirestore(app);
-// export {firestore};
+let analytics;
+if (typeof window !== 'undefined') {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  }).catch(console.error);
+}
+
+export { firestore, analytics };
+
